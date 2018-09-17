763b2c48a4582d7c6c44ea1b1850d94d34263c4a
ppoz21 <ppoz212@gmail.com>
2018-09-17 10:44:35 +0200

    1.0

---
+++ ulubione_potrawy.py
@@ -0,0 +1,11 @@
+# Ulubione potrawy
+# Program torzy jedną potrawę łączącą dwie wprowadzone przez użytkownika poterawy
+
+potrawa1 = input("Podaj jedną z twoich ulubionych potraw: ")
+potrawa2 = input("Podaj drugą z twoich ulubionych potraw: ")
+
+przysmak = potrawa1 + "-" + potrawa2
+
+print("Twoja nowa potrawa to: " + przysmak)
+
+input("\a\n\n Naciśnij enter")
