3bb5204ef6424d41a48b1c3ceaa2837e006bb624
ppoz21 <ppoz212@gmail.com>
2018-09-23 18:16:28 +0200

    1.0

---
+++ wybredny_licznik.py
@@ -0,0 +1,16 @@
+#Wybredny licznik
+#Demonstruje instrukcje break i continue
+
+count = 0
+
+while True:
+    count += 1
+    #zakończ pętlę, jeśli wartość zmiennej count jest większa niż 10
+    if count >10:
+        break
+    #pomic liczbę 5
+    if count == 5:
+        continue
+    print(count)
+
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
