b62907a511b7dc855cc54db19f4c71366bc1f930
ppoz21 <ppoz212@gmail.com>
2018-09-23 17:55:53 +0200

    1.0

---
+++ symulator_trzylatka.py
@@ -0,0 +1,12 @@
+#Symulator trzylatka
+#Demonstruje pentlę while
+
+print("\tWitaj w 'Symulatorze trzylatka'\n")
+print("Ten program symuluje rozmowę z trzyletnim dzieckiem.")
+print("Spróbuj przerwać to szaleństwo.\n")
+
+response = ""
+while response != "Bo tak temu":
+    response = input("Czemu?\n")
+print("Powiem mamie!")
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
